ea804e9767073ef79a710f56a45cb0e404637c15
Zbigniew Górawski <zgorawski@gmail.com>
2016-07-06 21:53:46 +0200

    changed structure of a project

---
+++ LoLSkins/Models/LoLChampion.swift
@@ -0,0 +1,13 @@
+//
+//  LoLChampion.swift
+//  LoLSkins
+//
+//  Created by Zbigniew Górawski on 06.07.2016.
+//  Copyright © 2016 Zbigniew Górawski. All rights reserved.
+//
+
+import Foundation
+
+struct LoLChampion {
+    
+}
